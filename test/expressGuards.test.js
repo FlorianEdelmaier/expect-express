1c16bb35588ea8d05858a3fd1d00e1d9e5c7f9f2
Florian Edelmaier <florian.edelmaier@gmail.com>
2016-08-17 13:39:35 +0200

    introduce node-mocks-http

--- test/expressGuards.test.js
+++ test/expressGuards.test.js
@@ -1,30 +1,36 @@
 'use strict';
 const expect = require('expect');
 const router = require('express').Router();
-const helper = require('./../lib/expressHelper');
+const guards = require('./../lib/expressGuards');
 
-describe('Helper', () => {
+describe('Express Guards', () => {
     describe('isRouter', () => {
         it('should validate express.Router', () => {
             let route = router.get('/', () => {});
-            expect(helper.isRouter(route)).toEqual(true);
+            expect(guards.isRouter(route)).toEqual(true);
         });
         it('should reject plain objects', () => {
-            expect(helper.isRouter({})).toEqual(false);
+            expect(guards.isRouter({})).toEqual(false);
         });
     });
     describe('isRoute', () => {
         it('should validate sub routes', () => {
             let routerTest = router.get('/', () => {});
-            expect(helper.isRoute(router.stack[0].route)).toEqual(true);
+            expect(guards.isRoute(router.stack[0].route)).toEqual(true);
         });
         it('should reject plain objects', () => {
-            expect(helper.isRoute({ path: '/' })).toEqual(false);
+            expect(guards.isRoute({ path: '/' })).toEqual(false);
         });
     });
     describe('isMiddleware', () => {
-        it('should validate middleware', () => {
-            expect(helper.isMiddleware((res, req, next) => {})).toEqual(true);
+        it('should validate middleware functions', () => {
+            expect(guards.isMiddleware((res, req, next) => {})).toEqual(true);
+        });
+        it('should fail if middleware is no function', () =>  {
+            expect(guards.isMiddleware({})).toEqual(false);
+        });
+        it('should fail if middleware does not accept 3 args', () => {
+            expect(guards.isMiddleware((res) => {})).toEqual(false);
         });
     });
     describe('getSubRoute', () => {
@@ -37,27 +43,27 @@ describe('Helper', () => {
             subRouter = router;
         });
         it('should filter to correct GET route', () => {
-            const sub = helper.getSubRoute(subRouter, 'GET', '/');
+            const sub = guards.getSubRoute(subRouter, 'GET', '/');
             expect(sub).toExist();
-            expect(helper.isRoute(sub.route)).toEqual(true);
+            expect(guards.isRoute(sub.route)).toEqual(true);
         });
         it('should filter to correct POST route', () => {
-            const sub = helper.getSubRoute(subRouter, 'POST', '/post');
+            const sub = guards.getSubRoute(subRouter, 'POST', '/post');
             expect(sub).toExist();
-            expect(helper.isRoute(sub.route)).toEqual(true);
+            expect(guards.isRoute(sub.route)).toEqual(true);
         });
         it('should filter to correct PUT route', () => {
-            const sub = helper.getSubRoute(subRouter, 'PUT', '/');
+            const sub = guards.getSubRoute(subRouter, 'PUT', '/');
             expect(sub).toExist();
-            expect(helper.isRoute(sub.route)).toEqual(true);
+            expect(guards.isRoute(sub.route)).toEqual(true);
         });
         it('should filter to correct DELETE route', () => {
-            const sub = helper.getSubRoute(subRouter, 'DELETE', '/del');
+            const sub = guards.getSubRoute(subRouter, 'DELETE', '/del');
             expect(sub).toExist();
-            expect(helper.isRoute(sub.route)).toExist();
+            expect(guards.isRoute(sub.route)).toExist();
         });
         it('should return undefined if not exist', () => {
-            expect(helper.getSubRoute(subRouter, 'GET', '/xxx/test')).toNotExist();
+            expect(guards.getSubRoute(subRouter, 'GET', '/xxx/test')).toNotExist();
         });
     });
 });
---
+++ test/mockHelper.test.js
@@ -0,0 +1,73 @@
+'use strict';
+const expect = require('expect');
+const router = require('express').Router();
+const helper = require('./../lib/mockHelper');
+
+describe('Mock helper', () => {
+    describe('getMockRequest', () => {
+        it('should return GET request object with params', () => {
+            const req = helper.getMockRequest('GET', '/test/:id', 'params', {id: 1});
+            expect(req).toBeAn('object');
+            expect(req.method).toEqual('GET');
+            expect(req.url).toEqual('/test/:id');
+            expect(req.params).toEqual({id: 1});
+        });
+        it('should return GET request object with query string', () => {
+            const req = helper.getMockRequest('GET', '/test', 'query', {id: 1});
+            expect(req).toBeAn('object');
+            expect(req.method).toEqual('GET');
+            expect(req.url).toEqual('/test');
+            expect(req.query).toEqual({id: 1});
+        });
+        it('should return GET request without any payload', () => {
+            const req = helper.getMockRequest('GET', '/test');
+            expect(req).toBeAn('object');
+            expect(req.method).toEqual('GET');
+            expect(req.url).toEqual('/test');
+            expect(req.query).toEqual({});
+            expect(req.params).toEqual({});
+        });
+        it('should return POST request object', () => {
+            const req = helper.getMockRequest('POST', '/test', 'body', { id: 1, name: 'test' });
+            expect(req).toBeAn('object');
+            expect(req.method).toEqual('POST');
+            expect(req.url).toEqual('/test');
+            expect(req.body).toEqual({ id: 1, name: 'test' });
+        });
+        it('should return PUT request object', () => {
+            const req = helper.getMockRequest('PUT', '/xxx', 'body', { id: 1, name: 'test' });
+            expect(req).toBeAn('object');
+            expect(req.method).toEqual('PUT');
+            expect(req.url).toEqual('/xxx');
+            expect(req.body).toEqual({ id: 1, name: 'test'});
+        });
+        it('should return DELETE request object', () => {
+            const req = helper.getMockRequest('DELETE', '/xxx', 'body', { id: 1 });
+            expect(req).toBeAn('object');
+            expect(req.method).toEqual('DELETE');
+            expect(req.url).toEqual('/xxx');
+            expect(req.body).toEqual({id: 1});
+        });
+        it('should throw error if payloadType is not supported', () => {
+            expect(() =>
+                helper.getMockRequest('GET', '/', 'test', {})
+            ).toThrow(/is not supported/);
+        });
+    });
+    describe('getMockResponse', () => {
+        it('should return response object', () => {
+            const resp = helper.getMockResponse();
+            expect(resp).toBeAn('object');
+            expect(resp.hasOwnProperty('statusCode')).toEqual(true);
+            expect(resp._getData).toBeA('function');
+        });
+    });
+    describe('getMockMiddleware', () => {
+        it('should return req/res mock and next spy', () => {
+            const mw = helper.getMockMiddleware('GET', '/');
+            expect(mw.req).toBeAn('object');
+            expect(mw.res).toBeAn('object');
+            expect(mw.nextSpy.__isSpy).toEqual(true);
+        });
+    });
+});
